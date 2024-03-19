294bc499fa71a1d169851566ac027180c253a8a1
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-03-19 16:55:03 +0100

    Add little CSS changes

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
   
   const startGameButton = document.querySelector('#start-game');
 
-  // Adds some basic click animation
-  startGameButton.addEventListener('mousedown', () => {
-    startGameButton.classList.add('clicked');
-  });
-  startGameButton.addEventListener('mouseup', () => {
-    startGameButton.classList.remove('clicked');
-  });
-
-  // Removes welcome page and starts the game
   startGameButton.addEventListener('click', () => {
     const gameHandler = new GameHandler();
     gameHandler.uiHandler.removeWelcome();
